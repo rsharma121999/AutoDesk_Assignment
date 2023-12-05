cb1e809d63c50a2b6770a7563cfc5b63cb404ade
rishabh_sharma <r.sharma121999@gmail.com>
2023-12-05 12:23:05 +0530

    Initial Test code added, testing is still required.

--- src/test/java/com/beta/replyservice/RestServiceApplicationTest.java
+++ src/test/java/com/beta/replyservice/RestServiceApplicationTest.java
@@ -1,14 +1,42 @@
 package com.beta.replyservice;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-public class RestServiceApplicationTest {
+@WebMvcTest(ReplyController.class)
+class RestServiceApplicationTest {
+
+	@MockBean
+	private ReplyService stringReplyService;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	public void contextLoads() {
+	void reply_ValidInput_ReturnsOk() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/reply/helloworld"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data").value("helloworld"));
 	}
 
+	@Test
+	void v2Reply_ValidInput_ReturnsOk() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/v2/reply/11-helloworld"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data").value("helloworld"));
+	}
+
+	@Test
+	void v2Reply_InvalidRule_ReturnsBadRequest() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/v2/reply/13-helloworld"))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.message").value("Invalid input"));
+	}
 }
